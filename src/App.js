e43e1c29f96b979ee273a65207e91fdff3faf652
rusairdrop <rusairdrop@gmail.com>
2021-11-26 20:38:23 +0300

    Delete button DONE!

--- src/App.js
+++ src/App.js
@@ -16,10 +16,21 @@ function App() {
     setPosts([...posts, newPost])
   }
   
+  const removePost = (post) => {
+    setPosts(posts.filter(item => item.id !== post.id))
+  }
+  
   return (
     <div className="App">
       <PostForm create={createPost}/>
-      <PostList posts={posts} title={'Список постов 1'}/>
+      {posts.length
+        ?
+        <PostList remove={removePost} posts={posts} title={'Список постов'}/>
+        :
+        <h1 style={{textAlign: 'center'}}>
+          Посты не найдены
+        </h1>
+      }
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MyButton from "./UI/button/MyButton";
 
 const PostItem = (props) => {
   
@@ -12,7 +13,9 @@ const PostItem = (props) => {
           </div>
         </div>
         <div className="post__btns">
-          <button>Удалить</button>
+          <MyButton onClick={() => props.remove(props.post)}>
+            Удалить
+          </MyButton>
         </div>
       </div>
     </div>
