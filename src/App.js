f285d13ce159cfb3d9b58c5cad0fc3e378fc3ff9
rusairdrop <rusairdrop@gmail.com>
2021-11-30 20:40:03 +0300

    Добавлен компонент модальрное окно Создание поста перенесено в модальное окно

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, {useMemo, useState} from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 import './styles/App.css'
-
 
 function App() {
   const [posts, setPosts] = useState([
@@ -14,17 +15,18 @@ function App() {
   ])
   
   const [filter, setFilter] = useState({sort: '', query: ''})
+  const [modal, setModal] = useState(false)
   
-  const
-    sortedPosts = useMemo(() => {
-      if (filter.sort) {
-        return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-      }
-      return posts;
-    }, [filter.sort, posts])
+  const sortedPosts = useMemo(() => {
+    if (filter.sort) {
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
+    }
+    return posts;
+  }, [filter.sort, posts])
   
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
   
   const sortedAndSearchedPosts = useMemo(() => {
@@ -37,7 +39,16 @@ function App() {
   
   return (
     <div className="App">
-      <PostForm create={createPost}/>
+      <MyButton
+        style={{marginTop: '15px'}}
+        onClick={() => setModal(true)}
+      >
+        Создать пост
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost}/>
+      </MyModal>
+      
       <hr style={{margin: '15px 0'}}/>
       <PostFilter
         filter={filter}
