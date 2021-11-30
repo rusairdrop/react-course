f111b2cea7aaa8faeb743dbd21a1186e2e91eb60
rusairdrop <rusairdrop@gmail.com>
2021-11-30 13:18:31 +0300

    добавлен компонент MySelect реализована сортировка постов

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 import './styles/App.css'
 
@@ -12,8 +13,15 @@ function App() {
     {id: 3, title: 'JavaScript 3', body: 'Description 3'},
   ])
   
+  const [selectedSort, SetSelectedSort] = useState('')
+  
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+  }
+  
+  const sortPosts = (sort) => {
+    SetSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
   
   const removePost = (post) => {
@@ -23,6 +31,18 @@ function App() {
   return (
     <div className="App">
       <PostForm create={createPost}/>
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue='Сортировка'
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'},
+          ]}
+        />
+      </div>
       {posts.length
         ?
         <PostList remove={removePost} posts={posts} title={'Список постов'}/>
